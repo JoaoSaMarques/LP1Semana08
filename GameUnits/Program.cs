60a42b62fc21604298795769e23a0e11c3be66e9
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-03 14:37:21 +0100

    Program

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -3,10 +3,21 @@
 namespace GameUnits
 {
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Unit unit1 = new MilitaryUnit(2, 10, 5);
+        Unit unit2 = new SettlerUnit();
+
+        //Unit 1 movement + health + cost
+        unit1.Move(3);
+        Console.WriteLine($"Unit 1 Health: {unit1.Health}");
+        Console.WriteLine($"Unit 1 Cost: {unit1.Cost}");
+
+        //Unit 2 movement + health + cost
+        unit2.Move(2);
+        Console.WriteLine($"Unit 2 Health: {unit2.Health}");
+        Console.WriteLine($"Unit 2 Cost: {unit2.Cost}");
     }
+}
 }
