a41b55738ff7f03e3dc7077fa894c1ed50d64142
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-03 14:21:39 +0100

    XP

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -4,9 +4,10 @@ namespace GameUnits
 {
     public class MilitaryUnit : Unit
     {
+        //Strength of attacks
         public int AttackPower { get; }
-        {
-            
-        }
+
+        //Xp of unit
+        public int XP { get; private set; }
     }
 }
