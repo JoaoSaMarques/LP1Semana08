72de7842dd4d7df2e556a75c01a4ad03ed74e9d3
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-03 14:35:29 +0100

    Settlerunit list

--- GameUnits/SettlerUnit.cs
+++ GameUnits/SettlerUnit.cs
@@ -4,6 +4,11 @@ namespace GameUnits
 {
     public class SettlerUnit : Unit
     {
+        //Overrides the cost
         public override float Cost => 5;
+    }
+
+    public SettlerUnit() : base(1, 2)
+    {
     }
 }
