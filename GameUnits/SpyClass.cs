d53725e23a32993fbec95eeafaf95f3a1eb7a282
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-05 10:29:58 +0100

    XP per unit type

--- GameUnits/SpyClass.cs
+++ GameUnits/SpyClass.cs
@@ -6,5 +6,28 @@ namespace GameUnits
     {
         //Override the cost of Unit
         public override float Cost => 12.5f;
+
+        //Get secret information
+        public void GetSecretInfo(Unit unit)
+        {
+            //If it engages with a spy unit
+            if (unit is SpyUnit)
+            {
+                //XP goes up by 3
+                XP += 3;
+            }
+            //If it engages with military unit then XP raises by 2
+            else if (unit is MilitaryUnit)
+            {
+                //XP goes up by 2
+                XP += 2;
+            }
+            //Otherwise if it's everything else(Like settler, +1 xp)
+            else
+            {
+                //XP goes up by 1
+                XP += 1;
+            }
+        }
     }
 }
