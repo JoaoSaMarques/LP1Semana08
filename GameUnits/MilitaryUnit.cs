9f0dc554adab775e9dda8a9cca170cdb6b244c66
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-05 10:26:54 +0100

    XP Unit instead of Unit for military class

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -2,16 +2,10 @@
 
 namespace GameUnits
 {
-    public class MilitaryUnit : Unit
+    public class MilitaryUnit : XPUnit
     {
         //Strength of attacks
         public int AttackPower { get; }
-
-        //Xp of unit
-        public int XP { get; private set; }
-
-        //Override cost to Strength + xp
-        public override float Cost => AttackPower + XP;
 
         //Constructor
         public MilitaryUnit(int movement, int health, int attackPower) : base(movement, health)
