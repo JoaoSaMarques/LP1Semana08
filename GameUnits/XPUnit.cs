0916ecbc3741f1a40ab64beca0805701c39f5abe
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-05 10:22:41 +0100

    Constructor

--- GameUnits/XPUnit.cs
+++ GameUnits/XPUnit.cs
@@ -6,5 +6,12 @@ namespace GameUnits
     {
         //XP
         public int XP { get; protected set; }
+
+        //Constructor
+        public XPUnit(int movement, int health) : base(movement, health)
+        {
+            //Set XP value
+            XP = 0;
+        }
     }
 }
