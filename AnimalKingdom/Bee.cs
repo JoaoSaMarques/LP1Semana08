90f036334aa434f54ffcdc4d1be1eeae98264a93
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-04-24 10:51:49 +0100

    wings for bat

--- AnimalKingdom/Bee.cs
+++ AnimalKingdom/Bee.cs
@@ -1,10 +1,12 @@
 ﻿namespace AnimalKingdom
 {
-    public class Bee
+    public class Bee : Animal, ICanFly
     {
         public override string Sound()
         {
             return base.Sound() + "Bzzz";
         }
+
+        public int NumberOfWings { get;} = 4;
     }
 }
