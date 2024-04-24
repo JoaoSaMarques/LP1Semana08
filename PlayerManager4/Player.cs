66b29027e2a9a1106afe2fc2ddac5e6793b0b52a
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-04-24 12:00:22 +0100

    added manager4

---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayerManager4
+{
+    public class Player : IComparable<Player>
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            // Compare the scores of the two players
+            return this.Score.CompareTo(other.Score);
+        }
+    }
+}
