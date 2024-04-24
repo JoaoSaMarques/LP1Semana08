9f587f2f5ae5e153d83a7b709a3432da0e238479
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-04-24 11:21:37 +0100

    Replaced program with moodle

---
+++ PlayerManager1/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace PlayerManager1
+{
+    public class Player
+    {
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        readonly string Name { get; }
+        public int Score { get; set; }
+    }
+    
+
+}
