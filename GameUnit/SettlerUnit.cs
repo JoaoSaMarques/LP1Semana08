01a53781e932f6c9ea01ca5299fe53c66bf4d2b6
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-09 14:29:41 +0100

    update base to have numbers instead

--- GameUnit/SettlerUnit.cs
+++ GameUnit/SettlerUnit.cs
@@ -2,13 +2,10 @@ namespace GameUnit
 {
     public class SettlerUnit : Unit // Heranca!!
     {
-        public SettlerUnit(int mov, int health) : base(mov, health)
-        {                                                        // Unit(int, int)
-            movement = 1;
-            health = 3;
-
+        public SettlerUnit(int mov, int health) : base(1, 3)
+        {                                         // movement = 1, health = 3
         }
 
-        public override float Cost => 5;
+        public override float Cost => 5; // Fixed cost of 5
     }
 }
