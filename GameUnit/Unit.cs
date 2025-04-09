d650471f5bcf68f5b68f5b0129498fdb7ea03189
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-09 14:16:33 +0100

    turn health into virtual int

--- GameUnit/Unit.cs
+++ GameUnit/Unit.cs
@@ -3,7 +3,7 @@ namespace GameUnit
     public class Unit
     {
         private readonly int movement;
-        public int Health { get; set; }
+        virtual int Health { get; set; }
 
         public Unit(int movement, int health)
         {
